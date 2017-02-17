003a4a761234d2d7c790f6704ad4366b24526862
chungw51993 <chungw51993@gmail.com>
2017-02-17 11:34:03 -0600

    Finish refactoring

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return(
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => 
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var List = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={["Cucumber", "Kale"]}/>
+  </div>
+);
+
+
+ReactDOM.render(<List />, document.getElementById("app"));
